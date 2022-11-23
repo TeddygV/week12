9756bbbaca86da2c22079f56adf8f71f1ceed1ef
Tibi <tvoinea@my.bcit.com>
2022-11-22 16:25:58 -0800

    this is the last commit of this week

--- week12/scripts/script.js
+++ week12/scripts/script.js
@@ -15,4 +15,57 @@ function changeBGPink()
 }
 clickButton.addEventListener("click", changeBGPink);
 
+function buttonTextChange() 
+{
+    clickButton.textContent = "Clicked!";
+    
+}
+clickButton.addEventListener("click", buttonTextChange);
 
+function changeText()
+{
+    if (clickButton.textContent === "Click Me!")
+    {
+        clickButton.textContent = "Clicked!";
+    } else if (clickButton.textContent === "Clicked!") {
+        clickButton.textContent = "Click Me!";
+    }
+}
+clickButton.addEventListener('click', changeText);function buttonTextChange() 
+{
+    clickButton.textContent = "Clicked!";
+    
+}
+clickButton.addEventListener("click", buttonTextChange);
+
+function changeText()
+{
+    if (clickButton.textContent === "Click Me!")
+    {
+        clickButton.textContent = "Clicked!";
+    } else if (clickButton.textContent === "Clicked!") {
+        clickButton.textContent = "Click Me!";
+    }
+}
+clickButton.addEventListener('click', changeText);
+
+function updateImage(event) 
+{
+    const image = document.querySelector("#shoppingCart");
+    image.src = "images/shoppingcart.png";
+    image.alt = "shopping cart";
+    image.width = "100";
+    image.height = "100";
+}
+
+clickButton.addEventListener('click', updateImage);
+
+const buttonContainer = document.querySelector(".div-container");
+buttonContainer.addEventListener('mouseover', changeBGGreen);
+
+function changeBGGreen(e)
+{
+    e.target.style.backgroundColor = "green";
+}
+
+buttonContainer.addEventListener("click", changeButtonTextColor);
